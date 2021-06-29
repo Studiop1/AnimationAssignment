f5b2ddf3b9e3c8b2eeb5e3198815301c6eeb3751
Studiop1 <85195210+Studiop1@users.noreply.github.com>
2021-06-29 18:20:16 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/git2/MainActivity.kt
+++ app/src/main/java/com/example/git2/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.git2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// This assignment is the assignment to do a simple pizza ordering app.
-// The app uses buttons, text fields and an image.
+// This assignment is the assignment to do a simple animation.
+// The button grows and shrinks looking a little like it bounces
 // You have a youtube video but also a 'shortcut' paper to explain how
 // Watch the video and then follow the steps in the paper ( or just
 // follow the video
